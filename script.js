6d52d2b7538507e99660251a718b695f0a386af9
abdurrafey-amir <abdurrafeyamir15@gmail.com>
2024-07-16 16:42:47 +0500

    made 1st version, particles move according to sound

---
+++ script.js
@@ -0,0 +1,40 @@
+let fft
+
+let Particle = function (position) {
+    console.log('called')
+    this.position = position
+    this.speed = createVector(0, 1)
+    this.color = [random(0, 255), random(0, 255), random(0, 255)]
+
+    this.draw = function() {
+        circle(this.position.x, this.position.y, this.diameter)
+        fill(this.color)
+    }
+    this.update = function (energy) {
+        this.diameter = random(5, 7) + energy * 100
+        this.position.y += this.speed.y * energy * 10
+        if (this.position.y > height) {
+            this.position.y = 0
+        }
+    }
+}
+
+function setup() {
+    createCanvas(windowWidth, windowHeight)
+    noStroke()
+
+    let mic = new p5.AudioIn()
+    mic.start()
+
+    fft = new p5.FFT()
+    fft.setInput(mic)
+
+    positionParticles()
+}
+
+function draw() {
+    background(0, 0, 0)
+    let spectrum = fft.analyze()
+    updateParticles(spectrum)
+
+}
